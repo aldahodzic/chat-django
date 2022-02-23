cff943d2b38f22d8be9fce09151706dfae628db0
aldahodzic <aldahodzic844@gmail.com>
2022-02-24 00:11:57 +1100

    Add default values to Message model.

--- chat/models.py
+++ chat/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 
 class User(models.Model):
@@ -9,10 +10,9 @@ class User(models.Model):
       return self.name
 
 
-# Create your models here.
 class Message(models.Model):
   body = models.CharField(max_length=255)
-  time_sent = models.DateTimeField()
-  is_read = models.BooleanField()
+  time_sent = models.DateTimeField(default=datetime.today)
+  is_read = models.BooleanField(default=False)
   sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_sender")
   recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_recipient")
