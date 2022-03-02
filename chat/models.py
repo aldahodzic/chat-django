e99073c7cdf2d1e5b8d20546dc1f6df8d6e0a461
aldahodzic <aldahodzic844@gmail.com>
2022-03-02 14:52:59 +1100

    Add NOT_NULL to model fields

--- chat/models.py
+++ chat/models.py
@@ -3,16 +3,16 @@ from django.utils.timezone import now
 
 
 class User(models.Model):
-  name = models.CharField(max_length=50)
-  hash = models.CharField(max_length=255)
+  name = models.CharField(max_length=50, null=False)
+  hash = models.CharField(max_length=255, null=False)
 
   def __str__(self):
       return self.name
 
 
 class Message(models.Model):
-  body = models.CharField(max_length=255)
-  time_sent = models.DateTimeField(default=now)
-  is_read = models.BooleanField(default=False)
-  sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_sender")
-  recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_recipient")
+  body = models.CharField(max_length=255, null=False)
+  time_sent = models.DateTimeField(default=now, null=False)
+  is_read = models.BooleanField(default=False, null=False)
+  sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_sender", null=False)
+  recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_recipient", null=False)
