ef3b11e53c1e0ee8e7747b1f690ecc095be924d2
aldahodzic <aldahodzic844@gmail.com>
2022-03-02 15:30:00 +1100

    Only execute json middleware on non-admin paths.

--- chat/middleware/requestmiddleware.py
+++ chat/middleware/requestmiddleware.py
@@ -5,14 +5,16 @@ def json_middleware(get_response):
 
   def middleware(request):
 
-    if (request.method == "POST" or request.method == "PUT"):
-      if (request.content_type == "application/json"):
-        try:
-          request.json = json.loads(request.body)
-        except:
-          return HttpResponse("Poorly formed JSON.",status=400)
-      else:
-        return HttpResponse("All POST / PUT requests must use JSON.", status=415)
+    if "/admin" not in request.get_full_path():
+
+      if (request.method == "POST" or request.method == "PUT"):
+        if (request.content_type == "application/json"):
+          try:
+            request.json = json.loads(request.body)
+          except:
+            return HttpResponse("Poorly formed JSON.",status=400)
+        else:
+          return HttpResponse("All POST / PUT requests must use JSON.", status=415)
 
     return get_response(request)
 
